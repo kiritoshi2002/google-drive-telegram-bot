9bb06a90a5013fac345672ff4b26410915a974e4
AnimeAddict01 <91471090+AnimeAddict01@users.noreply.github.com>
2021-09-27 22:31:11 +0900

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 class config:
     BOT_TOKEN = "1823594939:AAGzTAFUCXxlOPjWKt1W9XvwLx1zGdnh4Aw"
-    APP_ID = "7873544"
-    API_HASH = "dc08f1e11166dc91ca9005bfeb1ff42d"
-    DATABASE_URL = "postgres://pjlwqqdjolyjiw:6efd27f7bd94de542c875e2a677131a59e894614abd6006d5b5094fb39c0706a@ec2-34-234-12-149.compute-1.amazonaws.com:5432/dcb3vgl2g4rutf"
-    SUDO_USERS = "1143648407" # Sepearted by space.
-    SUPPORT_CHAT_LINK = "https://viperadnan-git.github.io"
+    APP_ID = "8258480"
+    API_HASH = "0d74c914d7bc6e5efe15eddf83ccbab1"
+    DATABASE_URL = "postgres://zydrpnmxmxkceu:9ea2371481450ca079135f5301a9fd56cd33403aafddc8903ba43f4c3c232e6d@ec2-34-199-209-37.compute-1.amazonaws.com:5432/d628pl7k3ti3ip"
+    SUDO_USERS = "1143648407 643888889" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/joinchat/Sxp8OyyTPHCwbFUx"
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
     G_DRIVE_CLIENT_SECRET = ""
